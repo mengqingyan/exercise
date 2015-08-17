8e7aeb7d405ea9d829db4819b6b6fdc85ad0e5c4
mengqingyan <m_527@qq.com>
2015-08-17 21:31:17 +0800

    move Exceptions.java to utils-package

--- quicksample/src/main/java/com/revencoft/sample/utils/Encodes.java
+++ quicksample/src/main/java/com/revencoft/sample/utils/Encodes.java
@@ -14,8 +14,6 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.StringEscapeUtils;
 
-import com.revencoft.sample.exceptions.Exceptions;
-
 /**
  * 封装各种格式的编码解码工具类.
  * 
